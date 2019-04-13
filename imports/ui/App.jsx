24cf61720fd919f0ef553ab2e966e88d8c33e26a
Franz Knipp <franz@qnipp.com>
2019-04-13 12:21:21 +0200

    Changing to accounts-ui

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import Hello from './Hello.jsx';
 import Info from './Info.jsx';
-
-import { Accounts } from 'meteor/epotek:accounts-ui';
-import { T9n } from 'meteor-accounts-t9n';
-import { en } from 'meteor-accounts-t9n/build/en'; // Choose the language you need here
-
-T9n.map('en', en);
-T9n.setLanguage('en');
-
-Accounts.config({
-  sendVerificationEmail: true,
-  forbidClientAccountCreation: false
-});
+import AccountsUIWrapper from './AccountsUIWrapper';
 
 export default App = () => (
   <div>
-    <Accounts.ui.LoginForm />
+    <AccountsUIWrapper />
     <h1>Willkommen zum Live Coding!</h1>
     <Hello />
     <Info />
